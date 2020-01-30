6b27121461bbfc4297c40c87dfb12446a675aebe
Andrii Revva <milaner.work@gmail.com>
2020-01-30 12:55:12 +0200

    hwdetect: Add USB to TTL device name into message
    Signed-off-by: Andrii Revva <milaner.work@gmail.com>

--- hwdetect/hwdetect.sh
+++ hwdetect/hwdetect.sh
@@ -4,7 +4,8 @@ check_usb_to_ttl() {
 	usb_to_ttl=$(ls /dev | grep -E "^ttyUSB[0-9]+$")
 	if [ -n "$usb_to_ttl" ];
 	then
-		echo "A USB to TTL convertor is connected."
+		echo "A USB to TTL convertor is connected:"
+		echo "${usb_to_ttl}"
 	fi
 }
 
