afb6e015763320b6ef28672e900ff817b4a75c68
Eduard Malokhvii <malokhvii.ee@gmail.com>
2020-01-31 17:20:48 +0200

    hwdetect: refactoring, extract some code fragments
    Signed-off-by: Eduard Malokhvii <malokhvii.ee@gmail.com>

--- hwdetect/hwdetect.sh
+++ hwdetect/hwdetect.sh
@@ -2,48 +2,137 @@
 
 readonly EXIT_OK=0
 
+get_device_major() {
+  local readonly device=$1
+  local readonly major=$(udevadm info ${device} | grep "MAJOR" | cut -d "=" -f2)
+
+  echo "${major}"
+}
+
+get_device_minor() {
+  local readonly device=$1
+  local readonly minor=$(udevadm info ${device} | grep "MINOR" | cut -d "=" -f2)
+
+  echo "${minor}"
+}
+
+get_block_devices() {
+  local devices=$(lsblk -dn | cut -d " " -f1 | sed -e 's#^#/dev/#' | sort)
+
+  echo "${devices}"
+}
+
+get_i2c_devices() {
+  local devices=()
+
+  for adapter in $(i2cdetect -l | grep -oP "i2c-\K(\d*)" | sort); do
+    for address in $(i2cdetect -y ${adapter} | grep -oP "\s\K(\d{2})\s" | sort); do
+      devices+=("/dev/i2c-${adapter}:0x${address}")
+    done
+  done
+
+  echo "${devices[@]}"
+}
+
+get_i2c_device_adapter() {
+  local readonly device=$1
+  local readonly adapter=$(echo ${device} | cut -d ":" -f1)
+
+  echo "${adapter}"
+}
+
+get_i2c_device_address() {
+  local readonly device=$1
+  local readonly address=$(echo ${device} | cut -d ":" -f2)
+
+  echo "${address}"
+}
+
+get_usb_to_ttl_convertors() {
+  local readonly devices=$(find /dev -name "ttyUSB*" | sort)
+
+  echo "${devices}"
+}
+
+get_usb_to_ttl_convertor_model() {
+  local readonly device=$1
+  local readonly model=$(
+    udevadm info ${device} | grep "ID_MODEL_FROM_DATABASE" | cut -d "=" -f2
+  )
+
+  echo "${model}"
+}
+
+print_block_devices() {
+  local readonly devices=($1)
+
+  if [[ -z "${devices}" ]]; then
+    return
+  fi
+
+  printf "BLOCK DEVICES\n"
+  printf "=============\n"
+  printf "%-15s %-10s\n" "DEVICE" "MAJ:MIN"
+
+  for device in "${devices[@]}"; do
+    local major=$(get_device_major ${device})
+    local minor=$(get_device_minor ${device})
+
+    printf "%-15s %-10s\n" "${device}" "${major}:${minor}"
+  done
+
+  echo
+}
+
+print_i2c_devices() {
+  local readonly devices=($1)
+
+  if [[ -z "${devices}" ]]; then
+    return
+  fi
+
+  printf "I2C DEVICES\n"
+  printf "===========\n"
+  printf "%-15s %-10s %s\n" "DEVICE" "MAJ:MIN" "ADDRESS"
+
+  for device in "${devices[@]}"; do
+    local adapter=$(get_i2c_device_adapter ${device})
+    local major=$(get_device_major ${adapter})
+    local minor=$(get_device_minor ${adapter})
+    local address=$(get_i2c_device_address ${device})
+
+    printf "%-15s %-10s %s\n" "${adapter}" "${major}:${minor}" "${address}"
+  done
+
+  echo
+}
+
 print_usb_to_ttl_convertors() {
+  local readonly devices=($1)
+
+  if [[ -z "${devices}" ]]; then
+    return
+  fi
+
   printf "USB TO TTL CONVERTORS\n"
   printf "=====================\n"
   printf "%-15s %-10s %-s\n" "DEVICE" "MAJ:MIN" "MODEL"
 
-  for dev in $(find /dev -name "ttyUSB*" | sort); do
-    local major=$(udevadm info ${dev} | grep "MAJOR" | cut -d "=" -f2)
-    local minor=$(udevadm info ${dev} | grep "MINOR" | cut -d "=" -f2)
-    local model=$(udevadm info ${dev} | grep "ID_MODEL_FROM_DATABASE" | cut -d "=" -f2)
+  for device in "${devices[@]}"; do
+    local major=$(get_device_major ${device})
+    local minor=$(get_device_minor ${device})
+    local model=$(get_usb_to_ttl_convertor_model ${device})
 
-    printf "%-15s %-10s %-s\n" "${dev}" "${major}:${minor}" "${model}"
+    printf "%-15s %-10s %-s\n" "${device}" "${major}:${minor}" "${model}"
   done
-}
 
-print_block_devices() {
-  printf "BLOCK DEVICES\n"
-  printf "=============\n"
-  printf "%-15s %-10s %-10s %-s\n" "NAME" "MAJ:MIN" "SIZE" "MOUNTPOINT"
-
-  lsblk -dn | awk '{ printf("%-15s %-10s %-10s %-s\n", $1, $2, $4, $7) }'
-}
-
-print_i2c_devices() {
-  printf "I2C DEVICES\n"
-  printf "===========\n"
-  printf "%-10s %s\n" "ADAPTER" "ADDRESS"
-
-  for adapter in $(i2cdetect -l | grep -oP "i2c-\K(\d*)" | sort); do
-    for address in $(i2cdetect -y ${adapter} | grep -oP "\s\K(\d{2})\s" | sort); do
-      printf "%-10s 0x%d\n" "i2c-${adapter}" "${address}"
-    done
-  done
+  echo
 }
 
 main() {
-  print_block_devices
-  echo
-
-  print_i2c_devices
-  echo
-
-  print_usb_to_ttl_convertors
+  print_block_devices "$(get_block_devices)"
+  print_i2c_devices "$(get_i2c_devices)"
+  print_usb_to_ttl_convertors "$(get_usb_to_ttl_convertors)"
 
   return ${EXIT_OK}
 }
