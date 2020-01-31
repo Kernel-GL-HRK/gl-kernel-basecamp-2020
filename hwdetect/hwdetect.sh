cbf9088dd0c1a5c9a6cd9c5609318a8433e1d7e1
Zorin Matvii <zorinmatvej87@gmail.com>
2020-01-31 18:04:15 +0200

    hwdetect: Optimize code 	Add check for empty ls log 	Add output into temporary file 	Rename variables
    Signed-off-by: Zorin Matvii <zorinmatvej87@gmail.com>

--- hwdetect/hwdetect.sh
+++ hwdetect/hwdetect.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
 #ls directory
-dir="/dev"
+dev_dir="/dev"
 
 #grep option
 opt="-w" #ignore slaves or holders
 
 #grep paterns
 ttyusb="ttyUSB['0'-'9']"
-flash="sd['b'-'z']" #ignore the first device - "sda"
-sdcard="mmcblk['0'-'9']"
+sd="sd['b'-'z']" #ignore the first device - "sda"
+mmcblk="mmcblk['0'-'9']"
 
-echo "Device directory: $dir"
+#bin directory
+bin_dir=./bin
+
+#ls logs
+tty_log=$(ls $dev_dir | grep $ttyusb)
+flash_log=$(ls $dev_dir | grep $opt $sd)
+mmc_log=$(ls $dev_dir | grep $opt $mmcblk)
+
+echo "Device directory: $dev_dir"
 echo "1 - TTY USB:"
-ls $dir | grep $ttyusb
+[[ -n $tty_log ]] && echo "$tty_log" || echo "none"
 echo "2 - Flash devices:"
-ls $dir | grep $opt $flash
+[[ -n $flash_log ]] && echo "$flash_log" || echo "none"
 echo "3 - SD cards:"
-ls $dir | grep $opt $sdcard
+[[ -n $mmc_log ]] && echo "$mmc_log" || echo "none"
+
+echo -e "$tty_log\n$flash_log\n$mmc_log" > $bin_dir/~temp1.txt
 
 exit $RETURN_SUCCES
