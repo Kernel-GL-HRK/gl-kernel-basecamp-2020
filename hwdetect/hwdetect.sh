8e31679509f40ab9a91e13f969ef1d72e0006dde
Dehtiarov Mykhailo <thegreatmike1062@gmail.com>
2020-01-31 23:24:10 +0000

    hwdetect: Add output formatting
    The script only outputs devices' names from /dev and an indicator
    whether they are plugged in or disconnected.
    
    Signed-off-by: Dehtiarov Mykhailo <thegreatmike1062@gmail.com>

--- hwdetect/hwdetect.sh
+++ hwdetect/hwdetect.sh
@@ -4,7 +4,7 @@ start_log=$(ls /dev)
 
 while true; do
     cur_log=$(ls /dev)
-    diff <(echo "$start_log") <(echo "$cur_log")
+    diff --new-line-format=$'--> %l\n' --old-line-format=$'<-- %l\n' --unchanged-group-format='' <(echo "$start_log") <(echo "$cur_log") 
     start_log=$cur_log
 done
 
