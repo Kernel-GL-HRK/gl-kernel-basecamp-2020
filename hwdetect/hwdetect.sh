ad2786fb4cda493ca2b77d24c6d29ece01f444a0
Kyryl Krynychnyi <mr.kkd22@gmail.com>
2020-02-02 23:52:04 +0200

    hwdetect: Add USB to TTL detector
    Create two tmp files. Redirect an output of ls /dev to these files and
    compare them. If the script is interrupted or exited, delete tmp files
    
    Signed-off-by: Kyryl Krynychnyi <mr.kkd22@gmail.com>

--- hwdetect/hwdetect.sh
+++ hwdetect/hwdetect.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-# USB to TTL convertors
+dev0=$(mktemp)
+ls /dev > $dev0
+dev1=$(mktemp)
+
+while true; do
+    ls /dev > $dev1
+    usb_to_ttl=$(diff $dev0 $dev1 | grep ttyUSB* )
+    if [[ -n  "$usb_to_ttl" ]]; then
+        if [[ "$(diff $dev0 $dev1 | grep ttyUSB* | head -c1)" = ">" ]]; then
+            echo "connected"
+        else
+            echo "disconnected"
+        fi  
+        echo "$usb_to_ttl"
+        echo
+        ls /dev > $dev0
+    fi
+done
+
+trap "rm -f $dev0" 0 2 3 15
+trap "rm -f $dev1" 0 2 3 15
 
 # flash drives
 
