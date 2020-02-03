701e4a3dc6bc131476cb75822aea7bdf48398568
CheeseMite <Cheese0Mite@gmail.com>
2020-02-03 16:49:54 +0200

    Add detection of new devices

--- hwdetect/hwdetect.sh
+++ hwdetect/hwdetect.sh
@@ -4,6 +4,20 @@ STATUS_OK=0
 
 function main
 {
+    devs=`ls /dev/`
+    difs=""
+
+    while [ true ]
+    do
+	    difs=`diff <(echo "$devs" ) <(echo "$(ls /dev/)") | grep '[<>]'`
+
+	
+	    if [ -n "$difs" ]
+    	then
+	    	sleep .5
+		    devs=`ls /dev/`
+	    fi	
+    done
     return $STATUS_OK
 }
 
