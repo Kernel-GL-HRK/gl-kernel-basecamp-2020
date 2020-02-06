f5cb2f602490f95d22806f9afa3514bc0953f543
Andrii Revva <milaner.work@gmail.com>
2020-02-06 09:50:17 +0200

    timer: Add dummy load attr and timer setup
    Added load attribute that return always 0%, and a timer setup with
    a function callback.
    
    Signed-off-by: Andrii Revva <milaner.work@gmail.com>

--- timer/mytimer.c
+++ timer/mytimer.c
@@ -1,21 +1,30 @@
 #include <linux/module.h>
 #include <linux/time.h>
+#include <linux/timer.h>
 #include <linux/fs.h>
 #include <linux/device.h>
 #include <linux/uaccess.h>
 
 #define CLASS_NAME "mytimer"
 #define MSG_LENGTH 128
+#define CPU_LOAD_UPDATE 1
 
 static struct timespec64 last_read;
+static u64 cpu_load;
+static struct timer_list cpu_load_timer;
 static struct class *mytimer_class;
 
 static ssize_t show_interval(struct class *class, struct class_attribute *attr, char *buf);
 
 static ssize_t show_time(struct class *class, struct class_attribute *attr, char *buf);
 
+static ssize_t show_load(struct class *class, struct class_attribute *attr, char *buf);
+
+static void cpu_load_callback(struct timer_list *timer);
+
 static struct class_attribute mytimer_interval = __ATTR(interval, 00644, show_interval, NULL);
 static struct class_attribute mytimer_time = __ATTR(time, 00644, show_time, NULL);
+static struct class_attribute mytimer_load = __ATTR(load, 00644, show_load, NULL);
 
 static ssize_t show_interval(struct class *class, struct class_attribute *attr, char *buffer)
 {
@@ -40,6 +49,20 @@ static ssize_t show_time(struct class *class, struct class_attribute *attr, char
 	return len;
 }
 
+static ssize_t show_load(struct class *class, struct class_attribute *attr, char *buffer)
+{
+	size_t len;
+	char buf[MSG_LENGTH];
+	len = sprintf(buf, "%llu%%", cpu_load);
+	strncpy(buffer, buf, len);
+	return len;
+}
+
+static void cpu_load_callback(struct timer_list *timer)
+{
+	printk(KERN_INFO "Timer: callback\n");
+}
+
 static int __init mytimer_init(void)
 {
 	int ret;
@@ -53,6 +76,13 @@ static int __init mytimer_init(void)
 		return ret;
 	if ((ret = class_create_file(mytimer_class, &mytimer_time)) < 0)
 		return ret;
+	if ((ret = class_create_file(mytimer_class, &mytimer_load)) < 0)
+		return ret;
+
+	/* Timer initialization */
+	timer_setup(&cpu_load_timer, cpu_load_callback, TIMER_DEFERRABLE);
+	cpu_load_timer.expires = get_jiffies_64() + (CPU_LOAD_UPDATE * HZ);
+	add_timer(&cpu_load_timer);
 	return 0;
 }
 
@@ -61,6 +91,7 @@ static void __exit mytimer_exit(void)
 	pr_info("mytimer: exit\n");
 	class_remove_file(mytimer_class, &mytimer_interval);
 	class_remove_file(mytimer_class, &mytimer_time);
+	class_remove_file(mytimer_class, &mytimer_load);
 	class_destroy(mytimer_class);
 }
 
