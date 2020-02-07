ccce55de2dae78df8d4f1a3e18c9914192af3fe4
Zorin Matvii <zorinmatvej87@gmail.com>
2020-02-07 23:19:21 +0200

    memory: Add 3 functions to provide memory allocate functions testing
    	See "Segmentation fault" in alloc_test() function
    
    Signed-off-by: Zorin Matvii <zorinmatvej87@gmail.com>

--- memory/uapi_memory.c
+++ memory/uapi_memory.c
@@ -8,7 +8,7 @@ long get_interval(struct timespec *ts1, struct timespec *ts2)
 	       (ts2->tv_nsec - ts1->tv_nsec);
 }
 
-int main(void)
+static inline void malloc_test(void)
 {
 	struct timespec ts1, ts2;
 	void *pdata = NULL;
@@ -18,7 +18,7 @@ int main(void)
 	long curr_free = 0;
 	long inter_count = 0;
 
-	printf("*--------------------------*malloc()*--------------------------*\n");
+	printf("\n*--------------------------*malloc()*--------------------------*\n");
 	printf("%-8s%-25s%-20s%-20s\n", "Power", "Buffer size (bytes)", "Allocation (ns)", "Freeing (ns)");
 	size_t alloc_size = 1;
 	do {
@@ -45,5 +45,80 @@ int main(void)
 	mean_alloc /= inter_count;
 	mean_free /= inter_count;
 	printf("\n%-33s%-20ld%-20ld\n", "MEAN", mean_alloc, mean_free);
+}
+
+static inline void calloc_test(void)
+{
+	struct timespec ts1, ts2;
+	void *pdata = NULL;
+	long mean_alloc = 0;
+	long mean_free = 0;
+	long curr_alloc = 0;
+	long curr_free = 0;
+	long inter_count = 0;
+
+	printf("\n*--------------------------*calloc()*--------------------------*\n");
+	printf("%-8s%-25s%-20s%-20s\n", "Power", "Buffer size (bytes)", "Allocation (ns)", "Freeing (ns)");
+	size_t alloc_size = 1;
+	do {
+		clock_gettime(CLOCK_REALTIME, &ts1);
+		pdata = calloc(alloc_size, 1);
+		clock_gettime(CLOCK_REALTIME, &ts2);
+
+		if (!pdata)
+			break;
+
+		curr_alloc = get_interval(&ts1, &ts2);
+		mean_alloc += curr_alloc;
+
+		clock_gettime(CLOCK_REALTIME, &ts1);
+		free(pdata);
+		clock_gettime(CLOCK_REALTIME, &ts2);
+
+		curr_free = get_interval(&ts1, &ts2);
+		mean_free += curr_free;
+		++inter_count;
+		printf("%-8ld%-25lu%-20ld%-20ld\n", inter_count, alloc_size, curr_alloc, curr_free);
+	} while(alloc_size *= 2);
+
+	mean_alloc /= inter_count;
+	mean_free /= inter_count;
+	printf("\n%-33s%-20ld%-20ld\n", "MEAN", mean_alloc, mean_free);
+}
+
+static inline void alloca_test(void)
+{
+	struct timespec ts1, ts2;
+	void *pdata = NULL;
+	long mean_alloc = 0;
+	long curr_alloc = 0;
+	long inter_count = 0;
+
+	printf("\n*--------------------------*alloca()*--------------------------*\n");
+	printf("%-8s%-25s%-20s\n", "Power", "Buffer size (bytes)", "Allocation (ns)");
+	size_t alloc_size = 1;
+	do {
+		clock_gettime(CLOCK_REALTIME, &ts1);
+		pdata = alloca(alloc_size);
+		clock_gettime(CLOCK_REALTIME, &ts2);
+
+		if (!pdata)
+			break;
+
+		curr_alloc = get_interval(&ts1, &ts2);
+		mean_alloc += curr_alloc;
+		++inter_count;
+		printf("%-8ld%-25lu%-20ld\n", inter_count, alloc_size, curr_alloc);
+	} while(alloc_size *= 2);
+
+	mean_alloc /= inter_count;
+	printf("\n%-33s%-20ld\n", "MEAN", mean_alloc);
+}
+
+int main(void)
+{
+	malloc_test();
+	calloc_test();
+	//alloca_test();
 	return 0;
 }
