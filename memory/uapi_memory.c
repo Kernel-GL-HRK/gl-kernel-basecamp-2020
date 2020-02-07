f4dea0f86383beed576d6d554e1efe621406ac0c
Zorin Matvii <zorinmatvej87@gmail.com>
2020-02-07 22:20:56 +0200

    memory: Add loop to allocate buffers
    	Measure time of functions malloc and free
    	Get average of execution time
    
    Signed-off-by: Zorin Matvii <zorinmatvej87@gmail.com>

--- memory/uapi_memory.c
+++ memory/uapi_memory.c
@@ -12,19 +12,38 @@ int main(void)
 {
 	struct timespec ts1, ts2;
 	void *pdata = NULL;
-	long inter = 0;
+	long mean_alloc = 0;
+	long mean_free = 0;
+	long curr_alloc = 0;
+	long curr_free = 0;
+	long inter_count = 0;
 
-	clock_gettime(CLOCK_REALTIME, &ts1);
-	pdata = malloc(1);
-	clock_gettime(CLOCK_REALTIME, &ts2);
-	inter = get_interval(&ts1, &ts2);
-	printf("malloc(1) inter = %ld ns\n", inter);
+	printf("*--------------------------*malloc()*--------------------------*\n");
+	printf("%-8s%-25s%-20s%-20s\n", "Power", "Buffer size (bytes)", "Allocation (ns)", "Freeing (ns)");
+	size_t alloc_size = 1;
+	do {
+		clock_gettime(CLOCK_REALTIME, &ts1);
+		pdata = malloc(alloc_size);
+		clock_gettime(CLOCK_REALTIME, &ts2);
 
-	clock_gettime(CLOCK_REALTIME, &ts1);
-	free(pdata);
-	clock_gettime(CLOCK_REALTIME, &ts2);
-	inter = get_interval(&ts1, &ts2);
-	printf("free() inter = %ld ns\n", inter);
+		if (!pdata)
+			break;
 
+		curr_alloc = get_interval(&ts1, &ts2);
+		mean_alloc += curr_alloc;
+
+		clock_gettime(CLOCK_REALTIME, &ts1);
+		free(pdata);
+		clock_gettime(CLOCK_REALTIME, &ts2);
+
+		curr_free = get_interval(&ts1, &ts2);
+		mean_free += curr_free;
+		++inter_count;
+		printf("%-8ld%-25lu%-20ld%-20ld\n", inter_count, alloc_size, curr_alloc, curr_free);
+	} while(alloc_size *= 2);
+
+	mean_alloc /= inter_count;
+	mean_free /= inter_count;
+	printf("\n%-33s%-20ld%-20ld\n", "MEAN", mean_alloc, mean_free);
 	return 0;
 }
