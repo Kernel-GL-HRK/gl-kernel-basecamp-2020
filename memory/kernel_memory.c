31cde62d0bf37315443761b27c4e03b12286be83
Zorin Matvii <zorinmatvej87@gmail.com>
2020-02-09 15:53:00 +0200

    kmemory: Add sysfs attribute to set allocated buffer size
    Signed-off-by: Zorin Matvii <zorinmatvej87@gmail.com>

--- memory/kernel_memory.c
+++ memory/kernel_memory.c
@@ -9,16 +9,38 @@
 
 #define BUFF_SIZE 1024
 
+static size_t alloc_size;
 static struct hrtimer hr_timer;
 static ktime_t kt1;
 static ktime_t kt2;
 static ktime_t alloc_time;
 
+static ssize_t alloc_size_show(struct class *class,
+			       struct class_attribute *attr, char *buffer);
+static ssize_t alloc_size_store(struct class *class,
+				struct class_attribute *attr,
+				const char *buffer, size_t count);
 static ssize_t kmalloc_show(struct class *class, struct class_attribute *attr,
 			    char *buffer);
 
 static struct class *class_alloc;
+CLASS_ATTR_RW(alloc_size);
 CLASS_ATTR_RO(kmalloc);
+
+static ssize_t alloc_size_show(struct class *class,
+			       struct class_attribute *attr, char *buffer)
+{
+	sprintf(buffer, "%lu\n", alloc_size);
+	return strlen(buffer);
+}
+
+static ssize_t alloc_size_store(struct class *class,
+				struct class_attribute *attr,
+				const char *buffer, size_t count)
+{
+	sscanf(buffer, "%lu\n", &alloc_size);
+	return count;
+}
 
 static ssize_t kmalloc_show(struct class *class, struct class_attribute *attr,
 			    char *buffer)
@@ -26,17 +48,17 @@ static ssize_t kmalloc_show(struct class *class, struct class_attribute *attr,
 	void *pdata;
 
 	kt1 = hrtimer_cb_get_time(&hr_timer);
-	pdata = kmalloc(BUFF_SIZE, GFP_KERNEL);
+	pdata = kmalloc(alloc_size, GFP_KERNEL);
 	kt2 = hrtimer_cb_get_time(&hr_timer);
 	if (!pdata) {
-		printk(KERN_WARNING "alloc-test: bad kmalloc: %d\n", ENOMEM);
+		printk(KERN_WARNING "kernel_memory: bad kmalloc: %d\n", ENOMEM);
 		return strlen(buffer);
 	}
 	alloc_time = kt2 - kt1;
 	kt1 = hrtimer_cb_get_time(&hr_timer);
 	kfree(pdata);
 	kt2 = hrtimer_cb_get_time(&hr_timer);
-	sprintf(buffer, "%-20llu%-20llu\n", alloc_time, (kt2 - kt1));
+	sprintf(buffer, "%llu %llu\n", alloc_time, (kt2 - kt1));
 
 	return strlen(buffer);
 }
@@ -44,18 +66,27 @@ static ssize_t kmalloc_show(struct class *class, struct class_attribute *attr,
 static int __init kernel_memory_init(void)
 {
 	int ret;
+	alloc_size = BUFF_SIZE;
 
-	class_alloc = class_create(THIS_MODULE, "alloc-test");
+	class_alloc = class_create(THIS_MODULE, "alloc_test");
 	if (IS_ERR(class_alloc)) {
 		ret = PTR_ERR(class_alloc);
-		printk(KERN_ERR "alloc-test: bad create sysfs class: %d\n",
+		printk(KERN_ERR "kernel_memory: bad create sysfs class: %d\n",
+		       ret);
+		return ret;
+	}
+
+	class_attr_alloc_size.attr.mode = (S_IWUGO | S_IRUGO);
+	ret = class_create_file(class_alloc, &class_attr_alloc_size);
+	if (ret) {
+		printk(KERN_ERR "kernel_memory: bad size attr create: %d\n",
 		       ret);
 		return ret;
 	}
 
 	ret = class_create_file(class_alloc, &class_attr_kmalloc);
 	if (ret) {
-		printk(KERN_ERR "alloc-test: bad kmalloc attr create: %d\n",
+		printk(KERN_ERR "kernel_memory: bad kmalloc attr create: %d\n",
 		       ret);
 		return ret;
 	}
@@ -63,20 +94,21 @@ static int __init kernel_memory_init(void)
 	hrtimer_init(&hr_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 	hrtimer_start(&hr_timer, KTIME_MAX, HRTIMER_MODE_REL);
 
-	printk(KERN_INFO "alloc-test: module loaded\n");
+	printk(KERN_INFO "kernel_memory: module loaded\n");
 	return 0;
 }
 
 static void __exit kernel_memory_exit(void)
 {
 	if (class_alloc) {
+		class_remove_file(class_alloc, &class_attr_alloc_size);
 		class_remove_file(class_alloc, &class_attr_kmalloc);
 	}
 	class_destroy(class_alloc);
 
 	hrtimer_cancel(&hr_timer);
 
-	printk(KERN_INFO "alloc-test: module unloaded\n");
+	printk(KERN_INFO "kernel_memory: module unloaded\n");
 }
 
 module_init(kernel_memory_init);
--- memory/memory_test.sh
+++ memory/memory_test.sh
@@ -1,13 +1,34 @@
  #!/bin/bash
 
+#For getting a random number in a range from $1 to $2
+function get_rand {
+        local temp=$RANDOM
+        local upper_bound=$1
+        local lower_bound=$2
+        let "temp = temp % (upper_bound - lower_bound) + lower_bound"
+        echo $temp
+}
+
+RANDOM=$SECONDS
+sys_dir=/sys/class/alloc_test
+start_upper=1024
+curr_upper=$start_upper
+curr_lower=0
+
 make format
 make
 make install
 printf "\n*--------------------------*kmalloc()*--------------------------*\n"
 printf "%-25s%-20s%-20s\n" "Buffer size (bytes)" "Allocation (ns)" "Freeing (ns)"
 for (( i = 0; i < 10; ++i)); do
-        printf "%-25lu%-40s\n" 1024 "$(cat /sys/class/alloc-test/kmalloc)"
+        rand_size=$(get_rand ${curr_upper} ${curr_lower})
+        curr_lower=$curr_upper
+        let "curr_upper += start_upper"
+        echo $rand_size > ${sys_dir}/alloc_size
+        kmalloc=( $(cat ${sys_dir}/kmalloc) )
+        printf "%-25lu%-20s%-20s\n" "$(cat ${sys_dir}/alloc_size)" ${kmalloc[0]} ${kmalloc[1]}
 done
+echo ""
 make uninstall
 
 exit $RETURN_SUCCESS
