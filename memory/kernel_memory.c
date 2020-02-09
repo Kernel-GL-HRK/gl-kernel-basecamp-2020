844eefa54381ab3ac8eb63721352618058fa7072
Zorin Matvii <zorinmatvej87@gmail.com>
2020-02-09 17:49:26 +0200

    kmemory: Add vmalloc testing
    	Introduce output redirection to the test script
    
    Signed-off-by: Zorin Matvii <zorinmatvej87@gmail.com>

--- memory/kernel_memory.c
+++ memory/kernel_memory.c
@@ -22,10 +22,13 @@ static ssize_t alloc_size_store(struct class *class,
 				const char *buffer, size_t count);
 static ssize_t kmalloc_show(struct class *class, struct class_attribute *attr,
 			    char *buffer);
+static ssize_t vmalloc_show(struct class *class, struct class_attribute *attr,
+			    char *buffer);
 
 static struct class *class_alloc;
 CLASS_ATTR_RW(alloc_size);
 CLASS_ATTR_RO(kmalloc);
+CLASS_ATTR_RO(vmalloc);
 
 static ssize_t alloc_size_show(struct class *class,
 			       struct class_attribute *attr, char *buffer)
@@ -63,6 +66,27 @@ static ssize_t kmalloc_show(struct class *class, struct class_attribute *attr,
 	return strlen(buffer);
 }
 
+static ssize_t vmalloc_show(struct class *class, struct class_attribute *attr,
+			    char *buffer)
+{
+	void *pdata;
+
+	kt1 = hrtimer_cb_get_time(&hr_timer);
+	pdata = vmalloc(alloc_size);
+	kt2 = hrtimer_cb_get_time(&hr_timer);
+	if (!pdata) {
+		printk(KERN_WARNING "kernel_memory: bad vmalloc: %d\n", ENOMEM);
+		return strlen(buffer);
+	}
+	alloc_time = kt2 - kt1;
+	kt1 = hrtimer_cb_get_time(&hr_timer);
+	vfree(pdata);
+	kt2 = hrtimer_cb_get_time(&hr_timer);
+	sprintf(buffer, "%llu %llu\n", alloc_time, (kt2 - kt1));
+
+	return strlen(buffer);
+}
+
 static int __init kernel_memory_init(void)
 {
 	int ret;
@@ -91,6 +115,13 @@ static int __init kernel_memory_init(void)
 		return ret;
 	}
 
+	ret = class_create_file(class_alloc, &class_attr_vmalloc);
+	if (ret) {
+		printk(KERN_ERR "kernel_memory: bad vmalloc attr create: %d\n",
+		       ret);
+		return ret;
+	}
+
 	hrtimer_init(&hr_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 	hrtimer_start(&hr_timer, KTIME_MAX, HRTIMER_MODE_REL);
 
@@ -103,6 +134,7 @@ static void __exit kernel_memory_exit(void)
 	if (class_alloc) {
 		class_remove_file(class_alloc, &class_attr_alloc_size);
 		class_remove_file(class_alloc, &class_attr_kmalloc);
+		class_remove_file(class_alloc, &class_attr_vmalloc);
 	}
 	class_destroy(class_alloc);
 
--- memory/memory_test.sh
+++ memory/memory_test.sh
@@ -11,24 +11,27 @@ function get_rand {
 
 RANDOM=$SECONDS
 sys_dir=/sys/class/alloc_test
-start_upper=1024
+start_upper=8192
 curr_upper=$start_upper
 curr_lower=0
 
 make format
 make
 make install
-printf "\n*--------------------------*kmalloc()*--------------------------*\n"
-printf "%-25s%-20s%-20s\n" "Buffer size (bytes)" "Allocation (ns)" "Freeing (ns)"
+echo "" > log.txt #Cleaning log file
+exec 3>log.txt 4>&1 1>&3 #Open to write new file descriptor, save stdout, stdout > log.txt
+printf "%-25s%-15s%-20s%-20s\n" "Buffer size (bytes)" "Memory" "Allocation (ns)" "Freeing (ns)"
 for (( i = 0; i < 10; ++i)); do
         rand_size=$(get_rand ${curr_upper} ${curr_lower})
         curr_lower=$curr_upper
         let "curr_upper += start_upper"
         echo $rand_size > ${sys_dir}/alloc_size
         kmalloc=( $(cat ${sys_dir}/kmalloc) )
-        printf "%-25lu%-20s%-20s\n" "$(cat ${sys_dir}/alloc_size)" ${kmalloc[0]} ${kmalloc[1]}
+        vmalloc=( $(cat ${sys_dir}/vmalloc) )
+        printf "\n%-25lu%-15s%-20s%-20s\n" "$(cat ${sys_dir}/alloc_size)" "kernel" ${kmalloc[0]} ${kmalloc[1]}
+        printf "%-25s%-15s%-20s%-20s\n" "" "virtual" ${vmalloc[0]} ${vmalloc[1]}
 done
-echo ""
+exec 1>&4 3>&- 4>&- #Return stdout, close log.txt, close temporary stdout
 make uninstall
 
 exit $RETURN_SUCCESS
