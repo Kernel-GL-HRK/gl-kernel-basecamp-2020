68c787bde1f35bf4f1d92fb396e729cf72727053
CheeseMite <Cheese0Mite@gmail.com>
2020-02-16 16:54:27 +0200

    mem1 : Add function to test Calloc

--- memory/mem.c
+++ memory/mem.c
@@ -36,8 +36,31 @@ int tryMalloc()
 	return 0;
 }
 
+int tryCalloc()
+{
+	static int npow;
+	void *pointer;
+	size_t size = pow(2.0, (double)npow++);
+
+	ticks = rdtsc();
+	pointer = (void *)calloc(1, size);
+	ticks = rdtsc() - ticks;
+
+	printf("Calloc %ld bytes lasts %d ticks\n", size, ticks);
+
+	if (pointer == NULL) {
+		printf("Bad calloc at %ld\n", size);
+		return -1;
+	}
+
+	free(pointer);
+
+	return 0;
+}
+
 int main()
 {
     while (!tryMalloc());
+    while (!tryCalloc());
 	return 0;
 }
